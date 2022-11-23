5c47db233802f24618610b4ae3cb2d96ecdc5587
Giacomo Frascarelli <frascarelli.1917888@studenti.uniroma1.it>
2022-11-23 12:56:25 +0100

    Commentato net_routing.py

--- src/routing_algorithms/net_routing.py
+++ src/routing_algorithms/net_routing.py
@@ -11,8 +11,20 @@ class MediumDispatcher:
         self.metric_class = metric_class
 
     def send_packet_to_medium(self, packet, src_drone, dst_drone, to_send_ts):
+        """
+        Simulates sending a packet from one drone to another with a specific timestamp, by inserting these values in
+        a list.
+        If the packet is a control packet, then increment the control packets counter by one.
 
+
+        @param packet: the packet to be sent
+        @param src_drone: the drone sending the packet
+        @param dst_drone: the drone receiving the packet
+        @param to_send_ts: current timestamp + delay
+        @return: None
+        """
         if isinstance(packet, DataPacket):
+            #TODO: figure this out: the function is incomplete, it just skips (pass instruction). Can we delete it or do we want to implement this?
 
             #self.metric_class.all_data_packets_in_simulation += 1
             pass
@@ -24,6 +36,22 @@ class MediumDispatcher:
         self.packets.append((packet, src_drone, dst_drone, to_send_ts))
 
     def run_medium(self, current_ts):
+        """
+        Delivers the messages found in the packets list to their respective destinations. The following checks are
+        done before performing a delivery:
+            - Current timestamp needs to be equal to the timestamp in the packet.
+            - The drones need to be inside their communication range.
+            - The packet must successfully traverse the medium channel (it has a small chance of being lost).
+
+        @param current_ts: current timestamp
+        @return: None
+        """
+
+        if self.subfunction1(current_ts) != self.subfunction2(current_ts):
+            print("List 1 ", self.subfunction1(current_ts))
+            print("List 2 ", self.subfunction2(current_ts))
+
+        # If a message is delivered remove it from the list of packets to send.
         to_drop_indices = []
         original_self_packets = self.packets[:]
         self.packets = []
@@ -40,5 +68,6 @@ class MediumDispatcher:
                         if dst_drone.routing_algorithm.channel_success(drones_distance):
                             dst_drone.routing_algorithm.drone_reception(src_drone, packet, current_ts)  # reception of a packet
 
+        # TODO: unroll this and make it more readable.
         original_self_packets = [original_self_packets[i] for i in range(len(original_self_packets)) if i not in to_drop_indices]
         self.packets = original_self_packets + self.packets
